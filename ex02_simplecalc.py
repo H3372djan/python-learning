eca1d376f6fc14527122b73b08d13b9ae1e70aa8
H3372djan <135830546+H3372djan@users.noreply.github.com>
2023-06-07 16:32:34 +0500

    Add files via upload

---
+++ ex02_simplecalc.py
@@ -0,0 +1,7 @@
+# This is my Simple Calculator
+a = int(input("input a number A:"))
+b = int(input("input a number B:"))
+print(f"A+B={a + b}")
+print(f"A-B={a - b}")
+print(f"AxB={a * b}")
+print(f"A/B={a / b}")
